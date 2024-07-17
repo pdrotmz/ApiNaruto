a1b963eb11388640b5904d3c5d79f1f1ec6d3d00
pdrotmz <pedrotomaz0210@gmail.com>
2024-07-17 01:48:44 -0300

    Initial commit

---
+++ src/test/java/dev/pdro/ApiNaruto/ApiNarutoApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.pdro.ApiNaruto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiNarutoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
