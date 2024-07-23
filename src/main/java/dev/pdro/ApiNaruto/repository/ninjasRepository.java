e2415d87e7495e5578f1008c7583e580d7f535fc
pdrotmz <pedrotomaz0210@gmail.com>
2024-07-22 22:00:50 -0300

    feat(repository): add import Ninjas model to ninjaRepository

--- src/main/java/dev/pdro/ApiNaruto/repository/ninjasRepository.java
+++ src/main/java/dev/pdro/ApiNaruto/repository/ninjasRepository.java
@@ -1,7 +1,8 @@
 package dev.pdro.ApiNaruto.repository;
 
+import dev.pdro.ApiNaruto.model.Ninjas;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ninjasRepository extends JpaRepository {
+public interface ninjasRepository extends JpaRepository<Ninjas, Long> {
 
 }
